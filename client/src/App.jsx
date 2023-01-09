5555a7936a99902104682e659e4cc53e247ad5c6
KuroUsagi2210 <115452905+KuroUsagi2210@users.noreply.github.com>
2023-01-09 10:35:43 +0000

    1er

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,27 +6,23 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((countParam) => countParam + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+<div>
+  <h1>Liste de course</h1>
+  <HomePage />
+  </div>
+
+  )
+}
+
+function HomePage(){
+  return( <div>
+    <ul>
+      <li> Schtroumpf 1<input type = "checkbox"/></li>
+      <li> Schtroumpf 2<input type = "checkbox"/></li>
+      <li> Schtroumpf 3<input type = "checkbox"/></li>
+    </ul>
+  </div>
+  )
 }
 
 export default App;
